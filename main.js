9e5924e9d9a76e27a1142713402a710e1ab41a04
John Love <john.love4@gmail.com>
2017-07-18 10:43:29 +0300

    add temperature conversion button

--- main.js
+++ main.js
@@ -14,14 +14,33 @@ $(document).ready(function(){
       $.getJSON(locationAPI, function(data){
 
         //get typeof of this.
-        $("#location").append("<p>In " +  data.results[0].formatted_address + "</p>");
+        $("#location").append("<p>In " +  data.results[4].formatted_address + "</p>");
       });
 
       $.getJSON(weatherAPI , function(data){
+        $("#weather").append("<p>The current temperature is " +  "<span id='temp'>" + Math.round(data.currently.temperature) + "</span>&deg; F</p>");
         console.log(data.currently.temperature)
       });
 
+      $("#change").on("click", function(){
+        var temp = $("#temp").text();
+        if(degree.celsius === true){
+          temp = Number(temp);
+          temp = (temp - 32) * 5/9;
+          temp = Math.round(temp);
+          $("#weather").empty().append("<p>The current temperature is " + "<span id='temp'>" + temp + "</span>&deg; C</p>");
+          degree.celsius = false;
+        } else {
+          temp = Number(temp);
+          temp = (temp * 9/5) + 32;
+          $("#weather").empty().append("<p>The current temperature is " + "<span id='temp'>" + temp + "</span>&deg; F</p>");
+          degree.celsius = true;
+        }
+      });
 
+      var degree = {
+        celsius: true,
+      };
 
     });
   }
